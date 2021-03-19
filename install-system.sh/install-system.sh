fbd0950e13da8a647335e87f87b22b871a29599b
Anshuman <amedhi@connect.ust.hk>
2021-03-19 16:48:39 +0800

    formatting in install-system.sh

--- install-system.sh/install-system.sh
+++ install-system.sh/install-system.sh
@@ -2,9 +2,14 @@ sudo apt-get dist-upgrade
 sudo apt-get install -y yadm
 yadm clone https://github.com/IndianBoy42/dotfiles.git
 
-sudo apt-get install -y apt-transport-https
-sudo apt-get install -y cmake ninja-build openocd
-sudo apt-get install -y curl build-essential gnupg2 lsb-release
+sudo apt-get install -y apt-transport-http\
+    cmake \
+    ninja-build \
+    openocd\
+    curl \
+    build-essential \
+    gnupg2 \
+    lsb-release
 
 wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | sudo apt-key add -
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
@@ -19,11 +24,40 @@ chsh -s /usr/bin/fish
 #fish -c "curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher"
 # Check if all fish plugins will be available already because of yadm
 
-sudo apt-get install -y  sublime-merge anydesk
-sudo apt-get install -y neovim x11-xserver-utils thefuck libudev-dev libusb-dev libcairo2-dev libjpeg-dev libgif-dev texlive-full python3-pip sublime-merge libpango1.0-dev librsvg2-dev libssl-dev libxcb1-dev libxcb-render0-dev libxcb-shape0-dev libxcb-xfixes0-dev
-sudo apt-get install -y libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6
-
-sudo apt-get install -y clang
+sudo apt-get install -y \
+    sublime-merge \
+    anydesk \
+    neovim \
+    x11-xserver-utils \
+    thefuck \
+    libudev-dev \
+    libusb-dev \
+    libcairo2-dev \
+    libjpeg-dev \
+    libgif-dev \
+    texlive-full \
+    python3-pip \
+    sublime-merge \
+    libpango1.0-dev \
+    librsvg2-dev \
+    libssl-dev \
+    libxcb1-dev \
+    libxcb-render0-dev \
+    libxcb-shape0-dev \
+    libxcb-xfixes0-dev \
+    libgl1-mesa-glx \
+    libegl1-mesa \
+    libxrandr2 \
+    libxrandr2 \
+    libxss1 \
+    libxcursor1 \
+    libxcomposite1 \
+    libasound2 \
+    libxi6 \
+    libxtst6 \
+    clang \
+    libclang-dev \
+    meson
 
 if not type -q rustup
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh

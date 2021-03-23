b9e4fcdd0b99ffb75ab56518c88edf01b19de9f2
Anshuman Medhi <amedhi@connect.ust.hk>
2021-03-24 00:34:14 +0800

    Create install-fisher.fish

---
+++ install-system.sh/install-fisher.fish
@@ -0,0 +1,10 @@
+#!/usr/bin/env fish
+
+fisher install IlanCosman/tide \
+ PatrickF1/fzf.fish \
+ edc/bass \
+ FabioAntunes/fish-nvm \
+ jorgebucaran/autopair.fish \
+ gazorby/fish-abbreviation-tips \
+ mattgreen/park.fish
+fisher update

03c78d4484e1aeafed042136f212b91b9f7fea06
Anshuman Medhi <amedhi@connect.ust.hk>
2021-03-23 23:51:02 +0800

    Update install-yadm.sh

--- install-system.sh/install-yadm.sh
+++ install-system.sh/install-yadm.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env sh
 # This probably will need to be run separately
+
 sudo apt-get install -y yadm
+
+wget https://github.com/microsoft/Git-Credential-Manager-Core/releases/download/v2.0.394-beta/gcmcore-linux_amd64.2.0.394.50751.deb -O /tmp/gcmcore.deb
+sudo dpkg -i /tmp/gcmcore.deb
+git-credential-manager-core configure
+git config --global credential.credentialStore secretservice # requires GUI (ok?)
+
 yadm clone https://github.com/IndianBoy42/dotfiles.git

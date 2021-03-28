5f716f1331537c18f1d0fc5196d1d48a8c3395c2
Anshuman <amedhi@connect.ust.hk>
2021-03-28 20:34:01 +0800

    update latex hsnips and install-fisher

--- install-system.sh/install-fish.sh
+++ install-system.sh/install-fish.sh
@@ -4,5 +4,4 @@ sudo apt-get install -y fish
 chsh -s /usr/bin/fish
 #fish -c "curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher"
 # Check if all fish plugins will be available already because of yadm
-fish -c "curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher"
 fish -c "source install-fisher.fish"
--- install-system.sh/install-fisher.fish
+++ install-system.sh/install-fisher.fish
@@ -1,5 +1,6 @@
 #!/usr/bin/env fish
 
+curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
 fisher install IlanCosman/tide \
  PatrickF1/fzf.fish \
  edc/bass \

e41315b757c939573d526564c7f36553ed91d4eb
Anshuman <amedhi@connect.ust.hk>
2021-04-01 01:42:41 +0800

    ghc and stack can be in the same file i guess

--- install-system.sh/install-ghc.fish
+++ install-system.sh/install-ghc.fish
@@ -2,7 +2,20 @@
 
 if not type -q ghcup
     curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
+else
+    ghcup
 end
 if not type -q stack
     curl -sSL https://get.haskellstack.org/ | sh
+else
+    stack upgrade
 end
+
+cd ~/git-builds/
+git clone --depth https://github.com/diku-dk/futhark.git
+cd futhark
+stack setup
+stack build
+stack install
+
+

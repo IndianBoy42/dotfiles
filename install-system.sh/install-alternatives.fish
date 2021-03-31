405188d5ad7e7d42130f58dea8d8099b0cab9b41
Anshuman <amedhi@connect.ust.hk>
2021-03-31 23:14:08 +0800

    add wezterm into install-alternatives (as default)

--- install-system.sh/install-alternatives.fish
+++ install-system.sh/install-alternatives.fish
@@ -1,4 +1,5 @@
 #!/usr/bin/env fish
 
-sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator (which alacritty) 99
+sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator (which alacritty) 90
+sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator (which wezterm) 99
 xdg-mime default zathura.desktop application/pdf 

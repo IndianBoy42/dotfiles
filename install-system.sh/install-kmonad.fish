9822184bf071d7b5b16140a8cbe176e89bdf98a7
Anshuman <amedhi@connect.ust.hk>
2021-03-31 17:47:57 +0800

    kmonad for awesome cross platform capslockmod wooo

---
+++ install-system.sh/install-kmonad.fish
@@ -0,0 +1,17 @@
+#!/usr/bin/env fish
+
+sudo apt install input-tools evtest
+wget https://github.com/david-janssen/kmonad/releases/download/0.4.1/kmonad-0.4.1-linux -O ~/bin/kmonad
+
+sudo groupadd uinput
+sudo usermod -aG uinput $USER
+sudo usermod -aG input $USER
+sudo modprobe uinput
+sudo touch /etc/udev/rules.d/uinput.rules
+echo 'KERNEL=="uinput", MODE="0660", GROUP="uinput", OPTIONS+="static_node=uinput"   (base)
+  ' | sudo tee /etc/udev/rules.d/uinput.rules
+  
+wget https://github.com/david-janssen/kmonad/raw/master/startup/kmonad.service -O /tmp/kmonad.service
+sudo cp kmonad.service /etc/systemd/system/kmonad.service
+sudo systemctl enable kmonad.service
+

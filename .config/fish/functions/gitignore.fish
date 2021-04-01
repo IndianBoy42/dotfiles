38b515d6bd7915203e2f57924b44946c726325f4
Anshuman <amedhi@connect.ust.hk>
2021-04-01 19:59:16 +0800

    fix gitignore dl script

--- .config/fish/functions/gitignore.fish
+++ .config/fish/functions/gitignore.fish
@@ -3,9 +3,10 @@ function gitignore
 	curl https://www.toptal.com/developers/gitignore/api/$argv > $tmpfile
 	cat $tmpfile | grep "!! ERROR"
 	set errlines (cat $tmpfile | grep "!! ERROR" | wc -l)
-	if [ $errlines -ge 0 ]
+	echo $errlines
+	if [ $errlines -gt 0 ]
 		echo "Not copying .gitignore as you have errors"
 	else 
-		cp $tmpfile .
+		cp $tmpfile ./.gitignore
 	end
 end

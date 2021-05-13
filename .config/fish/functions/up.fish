d52ad5c3f4d55fee781b70bf3bcfced1af3b8392
Anshuman <amedhi@connect.ust.hk>
2021-05-13 21:16:20 +0800

    up jump command

---
+++ .config/fish/functions/up.fish
@@ -0,0 +1,32 @@
+function up
+  set -l dir_to_be_child $argv[1]
+
+  if [ "$dir_to_be_child" = "" ]
+    cd ..
+  else
+    cd -
+    set -l alt_dir $PWD
+    cd -
+    set -l curr_dir $PWD
+
+    set -l is_found 0
+    while [ "$PWD" != "/" ]; and test $is_found -eq 0
+      cd ..
+      set -l res (find $dir_to_be_child -maxdepth 0 -type d 2>/dev/null | wc -l)
+      if test $res -eq 1
+        set is_found 1
+      end
+    end
+
+    if [ "$PWD" = "/" ]
+      cd $alt_dir
+      cd $curr_dir
+      echo "Couldn't find $dir_to_be_child"
+      return 1
+    end
+
+    set -l found_dir $PWD
+    cd $curr_dir
+    cd $found_dir
+  end
+end

91aa3916837a3d2d1c298e878b5df31b2d4290de
Anshuman <amedhi@connect.ust.hk>
2021-05-19 00:34:57 +0800

    refactor apt install into 2 files
    sources are in one file

---
+++ install-system.sh/install-apt-srcs.fish
@@ -0,0 +1,26 @@
+#!/usr/bin/env fish
+
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add - if not test -f /etc/apt/sources.list.d/sublime-text.list
+    echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+end
+wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg
+sudo mv signal-desktop-keyring.gpg /usr/share/keyrings/
+
+sudo add-apt-repository -y ppa:git-core/ppa
+echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' |\
+  sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo mv bazel.gpg /etc/apt/trusted.gpg.d/
+echo "deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
+
+sudo rm /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu groovy stable"
+
+sudo cp deb-experimental.list /etc/apt/sources.list.d
+sudo apt-key adv --keyserver   keyserver.ubuntu.com --recv-keys 7638D0442B90D010
+sudo apt-key adv --keyserver   keyserver.ubuntu.com --recv-keys 04EE7237B7D453EC
+
+sudo apt update
+

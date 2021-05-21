c8b81da3820c5d0a70c8e8c5c04cf2346f98bda8
Anshuman <amedhi@connect.ust.hk>
2021-05-21 13:07:35 +0800

    emacs

--- install-system.sh/install-emacs.fish
+++ install-system.sh/install-emacs.fish
@@ -3,8 +3,12 @@
 cd ~/git-builds
 git clone --depth 1 git://git.sv.gnu.org/emacs.git
 cd emacs
-sudo apt install libxaw7-dev libgnutls30 xaw3dg xutils-dev libgnutls28-dev
-./autogen.sh && ./configure && make
-checkinstall prefix=~/.local/
+sudo apt install libxaw7-dev libgnutls30 xaw3dg xutils-dev libgnutls28-dev libgccjit-10-dev
+./autogen.sh &&\
+./configure --with-native-compilation --with-modules=on &&\
+make -C lisp autoloads-force &&\
+make &&\
+sudo checkinstall prefix=$HOME/.local/
+
 git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
 ~/.emacs.d/bin/doom install

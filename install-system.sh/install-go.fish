4a3c2d2a32b425b01a3c56c2f07ffeffa793fedb
Anshuman <amedhi@connect.ust.hk>
2021-05-25 21:35:34 +0800

    install the fish gvm from here as well
    for others who want to use my install scripts

--- install-system.sh/install-go.fish
+++ install-system.sh/install-go.fish
@@ -4,5 +4,9 @@ if not type -q gvm
 	set tmpfile (mktemp)
 	curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer > $tmpfile
 	bash $tmpfile
+    function gvm
+      bass source ~/.gvm/scripts/gvm ';' gvm $argv
+    end
+    funcsave gvm
 end
 gvm install go1.16 -B && gvm use go1.16 --default

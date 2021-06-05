7dace1a36d06d5990166ec2a54c014f28746184a
Anshuman <amedhi@connect.ust.hk>
2021-06-05 21:32:46 +0800

    use github-cli to download nerdfonts

--- install-system.sh/install-nf.fish
+++ install-system.sh/install-nf.fish
@@ -1,14 +1,27 @@
 #!/usr/bin/env fish
 
 function nf-install-helper
-mkdir -p /tmp/fonts
-echo "[-] Download fonts [-]"
-echo "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/$argv.zip"
-set tmpfile (mktemp)
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/$argv.zip -O $tmpfile && unzip $tmpfile -d ~/.fonts
-fc-cache -fv
-echo "done!"
+    echo "[-] Download font ($argv) [-]"
+
+    mkdir -p ~/Downloads/fonts
+    cd ~/Downloads/fonts
+
+    set fontfilename "$argv.zip"
+
+    # set link "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/$argv.zip"
+    # echo "$link"
+    # wget $link -O $fontfilename
+    cd ~/Downloads/fonts
+    gh release -R ryanoasis/nerd-fonts download -p $fontfilename
+
+    and unzip $fontfilename -d ~/.fonts
+
+    echo "done!"
 end
 
 nf-install-helper FiraCode
+nf-install-helper Hack
 nf-install-helper FiraMono
+
+fc-cache -fv
+

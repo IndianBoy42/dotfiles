2822751aab989113e720ec8c8bd0e96e51b037b7
Anshuman <amedhi@connect.ust.hk>
2021-06-08 21:53:08 +0800

    fix fish install script

--- install-system.sh/install-fish.sh
+++ install-system.sh/install-fish.sh
@@ -5,5 +5,5 @@ sudo apt-add-repository ppa:fish-shell/release-3 && sudo apt-get install fish
 chsh -s /usr/bin/fish
 #fish -c "curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher"
 # Check if all fish plugins will be available already because of yadm
-fish -c "curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher && fisher update"
+fish -c "curl -sL https://git.io/fisher | source && fisher update"
 fish

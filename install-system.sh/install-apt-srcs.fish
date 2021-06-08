2c87dd14dbc7a03a9fced852165dbc47639639bc
Anshuman <amedhi@connect.ust.hk>
2021-06-08 22:23:03 +0800

    fix install-apt-srcs
    remove bazel, rearrange, remove '-a' flag from tee

--- install-system.sh/install-apt-srcs.fish
+++ install-system.sh/install-apt-srcs.fish
@@ -3,19 +3,20 @@
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch="(dpkg --print-architecture)" signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add - if not test -f /etc/apt/sources.list.d/sublime-text.list
+sudo add-apt-repository -y ppa:git-core/ppa
+
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add - 
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 
 wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg
 sudo mv signal-desktop-keyring.gpg /usr/share/keyrings/ 
-sudo add-apt-repository -y ppa:git-core/ppa
 echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' |\
-  sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
+  sudo tee /etc/apt/sources.list.d/signal-xenial.list
+
+# sudo mv bazel.gpg /etc/apt/trusted.gpg.d/
+# echo "deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo mv bazel.gpg /etc/apt/trusted.gpg.d/
-echo "deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
-
 sudo rm /usr/share/keyrings/docker-archive-keyring.gpg
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu groovy stable"

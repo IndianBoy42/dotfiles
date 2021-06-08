36140217c6ae26a188fd0d7145fe5a8b295a3099
Anshuman <amedhi@connect.ust.hk>
2021-06-09 02:13:15 +0800

    new bootstrap yadm

--- install-system.sh/install-yadm.sh
+++ install-system.sh/install-yadm.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 # This probably will need to be run separately
 
-sudo apt-get install yadm
-
-yadm clone https://github.com/IndianBoy42/dotfiles.git
+# Use a temporary yadm to clone the dotfiles
+curl -fLo /tmp/yadm https://github.com/TheLocehiliosan/yadm/raw/master/yadm && chmod +x /tmp/yadm
+/tmp/yadm clone https://github.com/IndianBoy42/dotfiles.git

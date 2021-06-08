56ebe506620ea991d0a2491b24c8f1390f6bd5cc
Anshuman <amedhi@connect.ust.hk>
2021-06-09 03:42:59 +0800

    add official bootstrap script

--- install-system.sh/install-yadm.sh
+++ install-system.sh/install-yadm.sh
@@ -4,3 +4,6 @@
 # Use a temporary yadm to clone the dotfiles
 curl -fLo /tmp/yadm https://github.com/TheLocehiliosan/yadm/raw/master/yadm && chmod +x /tmp/yadm
 /tmp/yadm clone https://github.com/IndianBoy42/dotfiles.git
+
+# CAN ALSO USE THEIR BOOTSTRAP 
+# curl -L bootstrap.yadm.io | bash -s -- https://github.com/IndianBoy42/dotfiles.git

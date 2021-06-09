d48a751a3b31587c18cb6bf80941012dc62024d4
Anshuman <amedhi@connect.ust.hk>
2021-06-09 15:13:45 +0800

    install a bunch of flatpaks

--- install-system.sh/misc/install-flatpak.fish
+++ install-system.sh/misc/install-flatpak.fish
@@ -4,4 +4,13 @@ if not type -q flatpak
     sudo apt install flatpak
     flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 end
-flatpak install com.spotify.Client com.anydesk.Anydesk org.gimp.GIMP org.inkscape.Inkscape
+flatpak install com.spotify.Client \
+com.anydesk.Anydesk \
+org.gimp.GIMP \
+org.inkscape.Inkscape \
+io.bit3.WhatsAppQT \
+org.kde.kdenlive \
+org.telegram \
+com.discordapp.Discord \
+org.signal.Signal \
+org.videolan.VLC

e241d00c13ab8f63b54c14828680ce8f868ff0b1
Anshuman <amedhi@connect.ust.hk>
2021-06-10 15:39:53 +0800

    more compact autols

--- .config/fish/functions/__reg_on_dir_change.fish
+++ .config/fish/functions/__reg_on_dir_change.fish
@@ -4,22 +4,21 @@ function __on_dir_change --on-variable PWD
     # if test $ON_DIR_CHANGE_EN -ne 0
     #     return 0
     # end
-	# TODO: dont onefetch if we are navigating around inside the repo
+
+    # Try get the current git repo
 	set _root (git rev-parse --show-toplevel 2> /dev/null)
 	if test -n "$_root"
-		if test -z "$CURR_GIT_ROOT"
+		if test -z "$CURR_GIT_ROOT" # Wasnt in a Git repo before
             git fetch &>/dev/null &
 			onefetch --no-color-palette 2> /dev/null
 		else 
-			if test "$CURR_GIT_ROOT" != "$_root"
+			if test "$CURR_GIT_ROOT" != "$_root" # Was in a different Git repo before
                 git fetch &>/dev/null &
 				onefetch --no-color-palette 2> /dev/null
 			end
 		end
 	end
 	set -gx CURR_GIT_ROOT "$_root"
-	# if test -d .git
-	# 	onefetch --no-color-palette 2> /dev/null
-	# end
-	lsd -A
+
+	lsd -A --color always --icon always --group-dirs first --timesort | head -5
 end

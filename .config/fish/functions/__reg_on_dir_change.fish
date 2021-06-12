79c1602a4ed02a6dc7363280a2b6871a725aeb25
Anshuman <amedhi@connect.ust.hk>
2021-06-12 16:20:05 +0800

    formatting

--- .config/fish/functions/__reg_on_dir_change.fish
+++ .config/fish/functions/__reg_on_dir_change.fish
@@ -5,21 +5,23 @@ function __on_dir_change --on-variable PWD
     #     return 0
     # end
 
+    # Print Git Repo info
     # Try get the current git repo
-	set _root (git rev-parse --show-toplevel 2> /dev/null)
-	if test -n "$_root"
-		if test -z "$CURR_GIT_ROOT" # Wasnt in a Git repo before
+    set _root (git rev-parse --show-toplevel 2> /dev/null)
+    if test -n "$_root"
+        if test -z "$CURR_GIT_ROOT" # Wasnt in a Git repo before
             git fetch &>/dev/null &
-			onefetch --no-color-palette 2> /dev/null
-		else 
-			if test "$CURR_GIT_ROOT" != "$_root" # Was in a different Git repo before
+            onefetch --no-color-palette 2> /dev/null
+        else 
+            if test "$CURR_GIT_ROOT" != "$_root" # Was in a different Git repo before
                 git fetch &>/dev/null &
-				onefetch --no-color-palette 2> /dev/null
-			end
-		end
-	end
-	set -gx CURR_GIT_ROOT "$_root"
+                onefetch --no-color-palette 2> /dev/null
+            end
+        end
+    end
+    set -gx CURR_GIT_ROOT "$_root"
 
+    # Print the Current Directory Summary
     if type -q ansicolumn
         lsd -A --color always --icon always --timesort \
             | ansicolumn -x \

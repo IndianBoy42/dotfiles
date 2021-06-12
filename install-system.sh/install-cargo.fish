bacca02d6cfd1e08935842a3ab6de17664a94bc8
Anshuman <amedhi@connect.ust.hk>
2021-06-12 16:21:05 +0800

    clean up (use musl more)

--- install-system.sh/install-cargo.fish
+++ install-system.sh/install-cargo.fish
@@ -3,6 +3,7 @@
 if not type -q rustup
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain nightly --profile default
     rustup target add x86_64-unknown-linux-musl      
+    rustup target add aarch64-unknown-linux-gnu      
     rustup target add thumbv7m-none-eabi
     rustup target add thumbv7em-none-eabi
     rustup target add thumbv7em-none-eabihf
@@ -13,11 +14,34 @@ else
     rustup update
 end
 
-CARGO_BUILD_TARGET=x86_64-unknown-linux-gnu cargo install  \
-    duma  \
+# TODO: use quickinstall (but no native compile :/)
+CARGO_BUILD_TARGET="x86_64-unknown-linux-gnu" \
+    cargo install cargo-quickinstall sccache
+
+type -q sccache && set -x RUSTC_WRAPPER (which sccache)
+
+set -x CARGO_BUILD_TARGET "x86_64-unknown-linux-gnu"
+# These cannot be compiled with musl (mostly due to OpenSSL-sys crate)
+cargo install \
+	alacritty \
+    cargo-edit \
+    cargo-generate \
+    cargo-outdated \
+    probe-run \
+    cargo-update \
+    spotify-tui \
+    so \
+    starship  \
+    tealdeer  \
+    duma  
+
+# TODO: static install and move to Justfile
+# S-tier: bat kn zoxide lsd bottom broot fd-find git-delta just ripgrep gitui
+# A-tier: macchina onefetch dust hyperfine  procs ptail pueue spotify-tui tealdeer starship watchexec-cli pipe-rename
+set -x CARGO_BUILD_TARGET "x86_64-unknown-linux-musl"
+cargo install \
     macchina \
 	onefetch \
-	alacritty \
 	kn \
     bandwhich  \
     bat  \
@@ -25,11 +49,8 @@ CARGO_BUILD_TARGET=x86_64-unknown-linux-gnu cargo install  \
     broot  \
     cargo-binutils \
     cargo-do \
-    cargo-edit \
-    cargo-generate \
-    cargo-outdated \
-    cargo-update \
     cargo-watch \
+    cargo-equip \
     drone  \
     du-dust \
     dua  \
@@ -58,42 +79,39 @@ CARGO_BUILD_TARGET=x86_64-unknown-linux-gnu cargo install  \
     sd  \
     shy  \
     skim \
-    so  \
-    spotify-tui \
-    starship  \
-    tealdeer  \
     verco  \
     watchexec-cli  \
     xsv  \
     zoxide \
     gpg-tui
 
-CARGO_BUILD_TARGET=x86_64-unknown-linux-gnu cargo install  \
-    --git https://github.com/IndianBoy42/gitui.git
+cargo install --git https://github.com/IndianBoy42/gitui.git
 
-CARGO_BUILD_TARGET=x86_64-unknown-linux-gnu cargo install  \
-    --git https://github.com/IndianBoy42/serial-monitor.git
+cargo install --git https://github.com/IndianBoy42/serial-monitor.git
 
-CARGO_BUILD_TARGET=x86_64-unknown-linux-gnu cargo install-update --all
+cargo install --git https://github.com/mosmeh/indexa.git
 
-pip install --user alacritty-colorscheme
-wget https://raw.githubusercontent.com/alacritty/alacritty/master/extra/logo/alacritty-term.svg -O /tmp/alacritty-term.svg
-sudo cp /tmp/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
-wget https://raw.githubusercontent.com/alacritty/alacritty/master/extra/alacritty.info -O /tmp/alacritty.info
-sudo tic -xe alacritty,alacritty-direct /tmp/alacritty.info
-wget https://raw.githubusercontent.com/alacritty/alacritty/master/extra/linux/Alacritty.desktop -O /tmp/Alacritty.desktop
-sudo desktop-file-install /tmp/Alacritty.desktop
-sudo update-desktop-database
-sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator (which alacritty) 99
+tldr --update
 
-sudo setcap cap_sys_ptrace,cap_dac_read_search,cap_net_raw,cap_net_admin+ep (which bandwhich)
+function post-cargo-install
+    pip install --user alacritty-colorscheme
+    wget https://raw.githubusercontent.com/alacritty/alacritty/master/extra/logo/alacritty-term.svg -O /tmp/alacritty-term.svg
+    sudo cp /tmp/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
+    wget https://raw.githubusercontent.com/alacritty/alacritty/master/extra/alacritty.info -O /tmp/alacritty.info
+    sudo tic -xe alacritty,alacritty-direct /tmp/alacritty.info
+    wget https://raw.githubusercontent.com/alacritty/alacritty/master/extra/linux/Alacritty.desktop -O /tmp/Alacritty.desktop
+    sudo desktop-file-install /tmp/Alacritty.desktop
+    sudo update-desktop-database
+    sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator (which alacritty) 99
 
-curl -L https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-linux -o ~/.local/bin/rust-analyzer;
-and chmod +x ~/.local/bin/rust-analyzer
+    sudo setcap cap_sys_ptrace,cap_dac_read_search,cap_net_raw,cap_net_admin+ep (which bandwhich)
+
+    curl -L https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-linux -o ~/.local/bin/rust-analyzer;
+    and chmod +x ~/.local/bin/rust-analyzer
 
 # vscode font for broot wget https://github.com/Canop/broot/raw/master/resources/icons/vscode/vscode.ttf -O ~/.local/share/fonts/vscode.ttf
 
-systemctl --user enable pueued.service
-and systemctl --user start pueued.service
+    systemctl --user enable pueued.service
+    and systemctl --user start pueued.service
 
-tldr --update
+end

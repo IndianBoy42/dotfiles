891f2d4161715645fd5d41b57aec09807b6f3f0e
Anshuman <amedhi@connect.ust.hk>
2021-06-13 15:03:01 +0800

    still broke

--- .config/fish/functions/rga-fzf-broken.fish
+++ .config/fish/functions/rga-fzf-broken.fish
@@ -1,15 +1,13 @@
 # Defined in /home/amedhi/.config/fish/config.fish @ line 221
 function rga-fzf-broken
 	set RG_PREFIX "rga --files-with-matches"
-	local file
-	file="(
+	set file (
 		FZF_DEFAULT_COMMAND="$RG_PREFIX '$1'" \
 			fzf --sort --preview="[[ ! -z {} ]] && rga --pretty --context 5 {q} {}" \
 				--phony -q "$1" \
 				--bind "change:reload:$RG_PREFIX {q}" \
 				--preview-window="70%:wrap"
-
-	)" &&
-	echo "opening $file" &&
-	xdg-open "$file"
+	) 
+	and echo "opening $file"
+	and xdg-open "$file"
 end

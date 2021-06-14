93588f24dd972d7a863566b34644f488d9be2f13
Anshuman <amedhi@connect.ust.hk>
2021-06-15 06:41:13 +0800

    better v command (support multiple args)

--- .config/fish/functions/v.fish
+++ .config/fish/functions/v.fish
@@ -1,8 +1,12 @@
-# Defined in /home/amedhi/.config/fish/config.fish @ line 131
+# Defined in /tmp/fish.RDtX7K/v.fish @ line 2
 function v
-	if test -f $argv
-		bat $argv
-	else
-		lsd -A $argv
-	end
+    test (count $argv) -lt 1 && lsd -A && return
+    for arg in $argv
+        if test -f $arg
+            lsd -al $arg
+            bat $arg
+        else
+            lsd -A $arg
+        end
+    end
 end

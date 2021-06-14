97bb4bc9c72f348fcbecf244b55a8724b32860f6
Anshuman <amedhi@connect.ust.hk>
2021-06-14 22:20:20 +0800

    support extract to

--- .config/fish/functions/extract.fish
+++ .config/fish/functions/extract.fish
@@ -1,24 +1,26 @@
-# Taken from: https://github.com/dideler/dotfiles/blob/master/functions/extract.fish
-
-function extract --description "Expand or extract bundled & compressed files"
+# Defined in /tmp/fish.kHdBpO/extract.fish @ line 2
+function extract --description 'Expand or extract bundled & compressed files'
   set --local ext (echo $argv[1] | awk -F. '{print $NF}')
+  test -n $argv[2];
+  and set --local tar_to "-C $argv[2]"
+  and set --local unzip_to "-d $argv[2]"
   switch $ext
     case tar  # non-compressed, just bundled
-      tar -xvf $argv[1]
+      tar -xvf $argv[1] $tar_to
     case gz
       if test (echo $argv[1] | awk -F. '{print $(NF-1)}') = tar  # tar bundle compressed with gzip
-        tar -zxvf $argv[1]
+        tar -zxvf $argv[1] $tar_to
       else  # single gzip
-        gunzip $argv[1]
+        gunzip $argv[1] $tar_to
       end
     case tgz  # same as tar.gz
-      tar -zxvf $argv[1]
+      tar -zxvf $argv[1] $tar_to
     case bz2  # tar compressed with bzip2
-      tar -jxvf $argv[1]
+      tar -jxvf $argv[1] $tar_to
     case rar
-      unrar x $argv[1]
+      unrar x $argv[1] $unzip_to
     case zip
-      unzip $argv[1]
+      unzip $argv[1] $unzip_to
     case '*'
       echo "unknown extension"
   end

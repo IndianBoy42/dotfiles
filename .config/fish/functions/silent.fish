62af45c9198472739c6f300cc9d641af19dec313
Anshuman <amedhi@connect.ust.hk>
2021-06-15 02:44:05 +0800

    silent command use pueue?

--- .config/fish/functions/silent.fish
+++ .config/fish/functions/silent.fish
@@ -1,4 +1,10 @@
 # Defined in /home/amedhi/.config/fish/config.fish @ line 96
-function silent
-	$argv &> /dev/null &
+if type -q pueue
+    function silent
+        pueue add "$argv"
+    end
+else 
+    function silent
+        $argv &> /dev/null &
+    end
 end

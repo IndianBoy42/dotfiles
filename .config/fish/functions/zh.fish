13e4ccd7b93ab4c8a27188a5510f4b06ae7f8982
Anshuman <amedhi@connect.ust.hk>
2021-06-15 19:13:10 +0800

    fix z history function

--- .config/fish/functions/zh.fish
+++ .config/fish/functions/zh.fish
@@ -1,4 +1,7 @@
-# Defined in /home/amedhi/.config/fish/config.fish @ line 185
+# Defined in /tmp/fish.ED0HfY/zh.fish @ line 2
 function zh
-	cd (dirh | fzf --tac | sd '\s*[0-9]+\)\s*(.*)' '$1')
+    set -l CHOICE (dirh | fzf --tac | sd '\s*[0-9]+\)\s*(.*)' '$1')
+    if test -n "$CHOICE"
+        cd $CHOICE
+    end
 end

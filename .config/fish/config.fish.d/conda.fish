331ce1aaf2d9f06f018b504b45a075125610a730
Anshuman <amedhi@connect.ust.hk>
2021-06-16 01:12:39 +0800

    a little more modular conda-abbrs

--- .config/fish/config.fish.d/conda.fish
+++ .config/fish/config.fish.d/conda.fish
@@ -1,8 +1,16 @@
 set CONDA_ENVS (conda env list --json | jq '.envs[] | select(test("env")) | split("/") | .[-1]' -r)
 set CONDA_ABBRS (conda env list --json | jq '.envs[] | select(test("env")) | split("/") | .[-1] | "py-\(.)"' -r)
-set CONDA_CMDS "install" "run" "update" "list" "remove" "clean" "update"
-for abbr in $CONDA_ENVS
+set -g CONDA_CMDS "install" "run" "update" "list" "remove" "clean" "update"
+function conda-abbrs
+    set abbr $argv
     for cmd in $CONDA_CMDS
         abbr -g "$abbr-$cmd" "conda $cmd -n $abbr"
     end
+end
+for abbr in $CONDA_ENVS
+    conda-abbrs $abbr
+end
+function conda-new-env 
+    conda create -n $argv python
+    conda-abbrs $argv[1]
 end

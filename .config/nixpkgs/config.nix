07b27b18c62a8bb49711b8359a0a9d704547dc42
Anshuman <amedhi@connect.ust.hk>
2021-06-15 08:34:02 +0800

    playing with nix declarative environments

--- .config/nixpkgs/config.nix
+++ .config/nixpkgs/config.nix
@@ -1,1 +1,14 @@
-{ allowUnfree = true; }
+{ 
+    allowUnfree = true;
+    packageOverrides = pkgs: with pkgs; rec {
+        stm-dev-env = pkgs.buildEnv {
+          name = "stm-dev-env";
+          paths = with pkgs; [
+            gcc-arm-embedded
+            openocd
+            ninja
+            cmake
+          ];
+        };
+    };
+}

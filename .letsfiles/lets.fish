6539d3f19df56dae22f6dbf01974ed46a6eafc36
Anshuman <amedhi@connect.ust.hk>
2021-06-15 23:56:48 +0800

    dont use a home working directory for Letsfile

--- .letsfiles/lets.fish
+++ .letsfiles/lets.fish
@@ -1,14 +1,21 @@
 # Defined interactively
 function lets
     if count $argv > /dev/null
+        # Run from a justfile if exists
         if test -e ~/.letsfiles/$argv[1].just
-            just --justfile ~/.letsfiles/$argv[1].just --working-directory ~ $argv[2..-1]
+            just --justfile ~/.letsfiles/$argv[1].just $argv[2..-1]
             return 0
         end
+        # Run a script/binary if exists
         if test -x ~/.letsfiles/$argv[1]
-            cd ~ && ~/.letsfiles/$argv[1]
+            cd ~ && ~/.letsfiles/$argv[1] $argv[2..-1]
+            return 0
+        end
+        # Run a nested script/binary if exists
+        if test -x ~/.letsfiles/$argv[1]/$argv[2]
+            cd ~ && ~/.letsfiles/$argv[1]/$argv[2] $argv[3..-1]
             return 0
         end
     end
-    just --justfile ~/.letsfiles/Justfile --working-directory ~ $argv
+    just --justfile ~/.letsfiles/Justfile $argv
 end

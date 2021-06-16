b407c1a39d46050c7026dfc3d113bba15103ba55
Anshuman <amedhi@connect.ust.hk>
2021-06-16 18:27:59 +0800

    tweak install-system.sh, do stuff that needs a reboot

--- install-system.sh/install-system.sh
+++ install-system.sh/install-system.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+set -euxo pipefail
+
 mkdir -p ~/bin
 
 # just upgrade everything once
@@ -21,3 +23,25 @@ mkdir -p ~/.local/libexec
 mkdir -p ~/.local/packages
 mkdir -p ~/.local/share/info
 chmod o+w ~/.local/share/info
+
+echo "Change the following:"
+echo"  34   │ #RuntimeDirectorySize=50%"
+echo"  35   │ #RuntimeDirectoryInodes=1000k"
+sudo nano /etc/systemd/logind.conf
+
+getent group uinput || sudo groupadd uinput
+sudo usermod -aG uinput $USER
+sudo usermod -aG input $USER
+sudo modprobe uinput
+sudo cp ./uinput.rules /etc/udev/rules.d/uinput.rules
+
+getent group docker || sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker 
+
+# Install fish
+./install-fish.sh
+
+read  -n 1 -p 'Reboot now? (you should):'
+echo ""
+sudo reboot now

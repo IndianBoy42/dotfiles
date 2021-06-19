a5e387a5e80322661ba5d4d0730447d08f5203ef
Anshuman <amedhi@connect.ust.hk>
2021-06-19 20:02:43 +0800

    v function also shows function definitions (and some headers)

--- .config/fish/functions/v.fish
+++ .config/fish/functions/v.fish
@@ -1,10 +1,14 @@
-# Defined in /tmp/fish.RDtX7K/v.fish @ line 2
+# Defined in /tmp/fish.h2mkZA/v.fish @ line 2
 function v
     test (count $argv) -lt 1 && lsd -A && return
     for arg in $argv
         if set -q $arg
             set --show $arg
-        else if test -f $arg
+        end
+        if type -q $arg
+            type -a $arg
+        end
+        if test -f $arg
             lsd -al $arg
             bat $arg
         else

56a4286d776793931fb8fe801b80e62ad8305b35
Anshuman <amedhi@connect.ust.hk>
2021-06-19 20:36:52 +0800

    forgot to commit

--- .config/fish/functions/v.fish
+++ .config/fish/functions/v.fish
@@ -1,17 +1,22 @@
-# Defined in /tmp/fish.h2mkZA/v.fish @ line 2
+# Defined in /tmp/fish.NWA88B/v.fish @ line 2
 function v
     test (count $argv) -lt 1 && lsd -A && return
     for arg in $argv
         if set -q $arg
+            echo "VARIABLES: "
             set --show $arg
         end
         if type -q $arg
+            echo "COMMANDS: "
             type -a $arg
         end
         if test -f $arg
+            echo "FILE: "
             lsd -al $arg
             bat $arg
-        else
+        end
+        if test -d $arg
+            echo "DIR LS: "
             lsd -A $arg
         end
     end

3f93e8d839f7030419ed73ef570825b8e89e028e
Anshuman <amedhi@connect.ust.hk>
2021-06-23 13:10:53 +0800

    conda activate special case

--- .config/fish/config.fish.d/conda.fish
+++ .config/fish/config.fish.d/conda.fish
@@ -1,15 +1,16 @@
 # set CONDA_ENVS (conda env list --json | jq '.envs[] | select(test("env")) | split("/") | .[-1]' -r)
 set CONDA_ENVS (find ~/anaconda3/envs/ -maxdepth 1 -type d | tail -n+2 | xargs -i basename "{}" )
 set EXTRA_CMDS "update" "list" 
-set CONDA_CMDS "install" "run" "remove" "clean" "update" "activate"
+set CONDA_CMDS "install" "run" "remove" "clean" "update"
 function conda-abbrs
-    set abbr $argv
+    set env $argv
     for cmd in $CONDA_CMDS
-        abbr -g "$abbr-$cmd" "conda $cmd -n $abbr"
+        abbr -g "$env-$cmd" "conda $cmd -n $env"
     end
+    abbr -g "$env-$cmd" "conda $cmd $env"
 end
-for abbr in $CONDA_ENVS
-    conda-abbrs $abbr
+for env in $CONDA_ENVS
+    conda-abbrs $env
 end
 function conda-new-env 
     conda create -n $argv python

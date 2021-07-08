cbf5ba2f165b4eaae61c01940578ec789c1d97a0
Anshuman <amedhi@connect.ust.hk>
2021-07-08 13:13:10 +0800

    fix startup issue

--- .config/fish/functions/fish_greeting.fish
+++ .config/fish/functions/fish_greeting.fish
@@ -1,12 +1,12 @@
 function fish_greeting
-# Reindex (indexa) if is-interactive
-	echo ""
-	if test -x ix
-		status --is-interactive; and ix -u &> /dev/null &
-	end
-	if type -q macchina
-		macchina
-	else
-		set_color red && echo "please cargo install macchina"
-	end
+    # Reindex (indexa) if is-interactive
+    # echo ""
+    # if test -x ix
+    # 	status --is-interactive; and ix -u &> /dev/null &
+    # end
+    # if type -q macchina
+    macchina </dev/null &
+    # else
+    # 	set_color red && echo "please cargo install macchina"
+    # end
 end

06eac8de1a82466855081dd5564e5044a0285ec3
Anshuman <amedhi@connect.ust.hk>
2021-07-19 01:15:53 +0800

    make touchp variadic

--- .config/fish/functions/touchp.fish
+++ .config/fish/functions/touchp.fish
@@ -1,5 +1,6 @@
-# Defined in /home/amedhi/.config/fish/config.fish @ line 212
 function touchp
-	mkdir -p (dirname $argv)
-	touch $argv
+    for arg in $argv
+        mkdir -p (dirname $arg)
+        touch $arg
+    end
 end

825027745be66d7dd5c82219c2c6f82d12cb97a3
Anshuman <amedhi@connect.ust.hk>
2021-11-15 11:31:02 +0800

    c-z to foreground

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,7 +1,7 @@
 # Defined in /home/amedhi/.config/fish/config.fish @ line 53
 function fish_user_key_bindings
-	bind -M insert \cg expand_glob
-	bind -M insert \cz 'kn'
-	bind -M insert \ez 'zi'
-	bind -M default cd kn
+    bind -M insert \cg expand_glob
+    bind \cz 'fg 2>/dev/null; commandline -f repaint'
+    bind -M insert \ez zi
+    bind -M default cd kn
 end

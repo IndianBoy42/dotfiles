0653347d9db3340c6c2ad3ebaee76c375610d0bb
Anshuman <amedhi@connect.ust.hk>
2021-11-17 00:54:01 +0800

    misc

--- .letsfiles/lets.fish
+++ .letsfiles/lets.fish
@@ -1,22 +1,25 @@
 # Defined interactively
 function lets
     set -l argc (count $argv)
-    if count $argv > /dev/null
+    if count $argv >/dev/null
         # Run from a justfile if exists
         if test -e ~/.letsfiles/$argv[1].just
             just -d . --justfile ~/.letsfiles/$argv[1].just $argv[2..-1]
             return 0
         end
-        # Run a script/binary if exists
-        if test -x ~/.letsfiles/$argv[1]
-            and not test -d ~/.letsfiles/$argv[1]
-            ~/.letsfiles/$argv[1] $argv[2..-1]
+        # Run a nested script/binary if exists
+        set -l nested ~/.letsfiles/$argv[1]/$argv[2]*
+        if test $argc -ge 2
+            and test -n "$nested"
+            and test -x "$nested"
+            $nested $argv[3..-1]
             return 0
         end
-        # Run a nested script/binary if exists
-        if test $argc -ge 2
-           and test -x ~/.letsfiles/$argv[1]/$argv[2]
-            ~/.letsfiles/$argv[1]/$argv[2] $argv[3..-1]
+        # Run a script/binary if exists
+        set -l script ~/.letsfiles/$argv[1]*
+        if test -n "$script"
+            and test -x "$script"
+            $script $argv[2..-1]
             return 0
         end
     end

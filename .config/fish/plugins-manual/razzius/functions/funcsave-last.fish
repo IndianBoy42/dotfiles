299ad99c4680400b0a8934e7d991cdbc0d8bfa17
Anshuman <amedhi@connect.ust.hk>
2021-11-26 20:05:05 +0800

    auto yadd

--- .config/fish/plugins-manual/razzius/functions/funcsave-last.fish
+++ .config/fish/plugins-manual/razzius/functions/funcsave-last.fish
@@ -2,4 +2,5 @@ function funcsave-last
     set function_name (last-function-name)
     funcsave $function_name
     echo Saved $function_name
+    yadd ~/.config/fish/functions/$function_name.fish
 end

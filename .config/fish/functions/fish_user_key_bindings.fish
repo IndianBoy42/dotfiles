8887a7448e81e0e55bbeb62c82a2c7f80d6ca52f
Anshuman <amedhi@connect.ust.hk>
2024-05-21 16:27:07 +0800

    zoxide expand on commandline

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,8 +1,18 @@
+function zi
+    commandline -rt (zoxide query --interactive (commandline -t))
+    or commandline -f repaint
+end
+function zc
+    commandline -rt (zoxide query (commandline -t))
+    or commandline -f repaint
+end
+
 function fish_user_key_bindings
     bind -M insert \cg expand_glob
     bind \cz 'fg 2>/dev/null; commandline -f repaint'
     bind -M insert \cz 'fg 2>/dev/null; commandline -f repaint'
     bind -M insert \ez zi
+    bind -M insert \eZ zc
     bind -M default cd kn
     bind -M insert ctrl-\; nvim
     bind -M default ctrl-\; nvim
